2289d67ad8789456185872e275e29edabb486c90
Sam Badman <samuelbadman@outlook.com>
2021-11-20 21:00:27 +0000

    Added tile type and contains entity properties to MapTileProperties

--- Assets/Scripts/Map/MapTileProperties.cs
+++ Assets/Scripts/Map/MapTileProperties.cs
@@ -3,12 +3,20 @@
 [Serializable]
 public class MapTileProperties
 {
+    public enum TileType
+    {
+        Unassigned,
+        Forest,
+        Rock, 
+        Village,
+        // TODO: Decide tile types
+    }
+
     public float Integrity = 1.0f;
+    public TileType Type = TileType.Unassigned;
+    public bool ContainsEntity = false;
 
     public bool Alive() { return Integrity <= 0.0f; }
 
-    // TODO: Contains entity
-    // TODO: Tile type menu
-    // TODO: Tile neighbours
     // TODO: Modular tile event types
 }
