1756818290aad4305731cb9014e31c2bfeda0f4a
Steve <steverobray@gmail.com>
2021-11-22 13:54:02 +0000

    Updates sound manager to have debug audio settings

--- Assets/Scripts/Core/SoundSystem.cs
+++ Assets/Scripts/Core/SoundSystem.cs
@@ -4,13 +4,19 @@ namespace Core
 {
     public class SoundSystem : MonoBehaviour
     {
+        private enum AudioMode { PlayAudio, DebugDisableAudio, DebugEffectsOnly }
+        [SerializeField] private AudioMode _audioMode = AudioMode.PlayAudio;
+        
         private static AudioSource _musicSource = default;
         private static AudioSource _effectSource = default;
         
         private void OnEnable()
         {
             // get the audio sources
-            var components = GetComponentsInChildren<AudioSource>(); 
+            var components = GetComponentsInChildren<AudioSource>();
+            
+            // set Audio Mode
+            SetDebugAudioMode();
             
             // set up music
             _musicSource = components[0];
@@ -32,6 +38,17 @@ namespace Core
         public static void PlayEffect(AudioClip clip)
         {
             _effectSource.PlayOneShot(clip);
-        } 
+        }
+
+        private void SetDebugAudioMode()
+        {
+            if (_audioMode == AudioMode.PlayAudio || !Application.isEditor) return;
+            
+            _musicSource.mute = true;
+            if (_audioMode != AudioMode.DebugEffectsOnly)
+            {
+                _effectSource.mute = true;
+            }
+        }
     }
 }
