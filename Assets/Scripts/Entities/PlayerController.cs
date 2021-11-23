59095caff3613f1d9c481cf183e1f3d908302a53
kieranbow <Kieran2.Bowsher@live.uwe.ac.uk>
2021-11-23 15:17:12 +0000

    Player can now move around the map using the mouse input.

--- Assets/Scripts/Entities/PlayerController.cs
+++ Assets/Scripts/Entities/PlayerController.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Player player;
-    public Map map;
-
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            MapCoordinate mapCoordinate = new MapCoordinate((int)Input.mousePosition.x, (int)Input.mousePosition.y);
-            map.IsValidCoordinate(mapCoordinate);
-            player.Move(map.GetTile(mapCoordinate).getLocation());
-            print("map coord:" + Input.mousePosition.x + Input.mousePosition.y);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 1000))
+            {
+                Vector3 position = transform.position;
+
+
+
+                position.x = Mathf.Floor(hit.point.x) + 0.5f;
+                position.y = Mathf.Floor(hit.point.y) + 1.0f; // Change this to be based on height
+                position.z = Mathf.Floor(hit.point.z) + 0.5f;
+
+                transform.position = position;
+
+                print(position);
+            }
+
         }
     }
 }
