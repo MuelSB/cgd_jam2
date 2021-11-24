fd62be923f883250c42381183b353d701d1f508f
Sam Badman <samuelbadman@outlook.com>
2021-11-24 13:20:25 +0000

    Added player constructor to initialize entity properties for the player

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -6,6 +6,13 @@ public class Player : Entity
 {
     public List<Items> items;
 
+    Player()
+    {
+        health = 100.0f;
+        type = EntityType.PLAYER;
+        movementRange = 5;
+    }
+
     public void TakeDamage(int damage)
     {
         health -= damage;
