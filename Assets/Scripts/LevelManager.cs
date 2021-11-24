6cc1d0cdbc96fb913b67be72ac1f8575da716327
Sam Badman <samuelbadman@outlook.com>
2021-11-24 13:29:30 +0000

    Added player test map

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -47,9 +47,5 @@ public class LevelManager : MonoBehaviour
 
         MapManager.CreateMap(mapCreateSettings,metaGeneratorConfig);
         var map = MapManager.GetMap();
-
-        var id = map.AddEventToTile(new TestMapTileEvent(), new MapCoordinate(0, 0));
-        map.ActivateTile(new MapCoordinate(0, 0));
-        map.RemoveEventFromTile(id, new MapCoordinate(0,0));
     }
 }
