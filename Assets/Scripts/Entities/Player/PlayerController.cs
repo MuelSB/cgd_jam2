cdad54b849f07841af8df7c9b266d25e71606729
Sam Badman <samuelbadman@outlook.com>
2021-11-24 18:13:54 +0000

    Added player movement to select input event

--- Assets/Scripts/Entities/Player/PlayerController.cs
+++ Assets/Scripts/Entities/Player/PlayerController.cs
@@ -7,24 +7,27 @@ public class PlayerController : MonoBehaviour
 {
     // Class variables
     PlayerInputActions playerInputActions;
-    private Maybe<Player> ControlledPlayer = new Maybe<Player>();
+    private Maybe<Player> controlledPlayer = new Maybe<Player>();
+    private float selectRayLength = 10000.0f;
+    private float playerMovementSmoothing = 0.01f;
+    private int tileLayerMask = 1 << 6;
 
     public void ControlPlayer(Player targetPlayer)
     {
         // Set the player that this controller is controlling
-        ControlledPlayer = new Maybe<Player>(targetPlayer);
+        controlledPlayer = new Maybe<Player>(targetPlayer);
 
         // Call the player's on controlled event
-        ControlledPlayer.value.OnControlled();
+        controlledPlayer.value.OnControlled();
     }
 
     public void UnControlPlayer()
     {
         // If this controller is not controlling a player, just return
-        if (!ControlledPlayer.is_some) return;
+        if (!controlledPlayer.is_some) return;
 
         // Call the controlled player's on uncontrolled event
-        ControlledPlayer.value.OnUnControlled();
+        controlledPlayer.value.OnUnControlled();
     }
 
     private void Awake()
@@ -42,31 +45,41 @@ public class PlayerController : MonoBehaviour
 
     private void Select(InputAction.CallbackContext context)
     {
+        // Raycast from the cursor world position down into the scene
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(
+                Mouse.current.position.x.ReadValue(),
+                Mouse.current.position.y.ReadValue(),
+                0.0f));
+        RaycastHit hit;
 
+        //Debug.DrawRay(ray.origin, ray.direction * selectRayLength, Color.red, 5.0f);
+
+        if (Physics.Raycast(ray, out hit, selectRayLength, tileLayerMask))
+        {
+            StartCoroutine(MovePlayerToTile(hit.collider.gameObject));
+        }
     }
 
-    //void Update()
-    //{
-    //    if (Input.GetMouseButton(0))
-    //    {
-    //        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-    //        RaycastHit hit;
+    private IEnumerator MovePlayerToTile(GameObject tileObject)
+    {
+        // Do nothing if the controller is not controlling a player
+        if (!controlledPlayer.is_some) yield return null;
 
-    //        if (Physics.Raycast(ray, out hit, 1000))
-    //        {
-    //            Vector3 position = transform.position;
+        // Compute target position
+        var playerCollisionHalfHeight = controlledPlayer.value.GetComponent<CapsuleCollider>().height * 0.5f;
+        var tileCollisionHalfHeight = tileObject.GetComponent<BoxCollider>().size.y * 0.5f;
 
+        var targetPosition = tileObject.transform.position;
+        targetPosition.y += playerCollisionHalfHeight + tileCollisionHalfHeight;
 
+        // Move the player towards the target position
+        while(Vector3.Distance(controlledPlayer.value.transform.position, targetPosition) > 0.005f)
+        {
+            controlledPlayer.value.transform.position = Vector3.Lerp(controlledPlayer.value.transform.position, targetPosition, playerMovementSmoothing * Time.time);
 
-    //            position.x = Mathf.Floor(hit.point.x) + 0.5f;
-    //            position.y = Mathf.Floor(hit.point.y) + 1.0f; // Change this to be based on height
-    //            position.z = Mathf.Floor(hit.point.z) + 0.5f;
+            yield return null;
+        }
 
-    //            transform.position = position;
-
-    //            print(position);
-    //        }
-
-    //    }
-    //}
+        yield return null;
+    }
 }
