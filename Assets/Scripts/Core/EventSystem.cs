6d7cca1afe251843de102d29e2aa61179f073013
Steve <steverobray@gmail.com>
2021-11-30 20:48:49 +0000

    Adds generic Param to event handler

--- Assets/Scripts/Core/EventSystem.cs
+++ Assets/Scripts/Core/EventSystem.cs
@@ -1,54 +1,52 @@
+using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace Core
 {
     public class EventSystem : MonoBehaviour
     {
-        // map for all events
-        private static Dictionary<string, UnityEvent> _events = new Dictionary<string, UnityEvent>();
-        
-        // delegate used as func parameter
-        public delegate void Fnc();
-        //public delegate void Fnc<in T>(T param);
-
-        public static void Subscribe(string name, Fnc fnc)
+        // event handlers
+        private static Dictionary<Type, IEventHandler> _genericHandlers = new Dictionary<Type, IEventHandler>();
+        private static EventHandler _eventHandler = new EventHandler();
+    
+        // no params 
+        public static void Subscribe(string key, Action func) => _eventHandler.Subscribe(key, func);
+        public static void Unsubscribe(string key, Action func) => _eventHandler.Unsubscribe(key, func);
+        public static void Invoke(string key) => _eventHandler.Invoke(key);
+    
+        // single param generics
+        public static void Subscribe<T>(string key, Action<T> func )
         {
-            // manages memory assignment 
-            if (!_events.ContainsKey(name))
+            // create a new handler when you use a new type
+            if (!_genericHandlers.ContainsKey(typeof(T)))
             {
-                _events[name] = new UnityEvent();
+                _genericHandlers[typeof(T)] = new EventHandler<T>();
             }
-            // subscribes the function
-            _events[name].AddListener(fnc.Invoke);
-        }
         
-        public static void Unsubscribe(string name, Fnc fnc)
-        {
-            // early exit if it doesnt exist
-            if (!_events.ContainsKey(name)) return;
-            
-            // unsubscribes 
-            _events[name].RemoveListener(fnc.Invoke);
-            
-            // removes mem if not being used
-            if (_events[name].GetPersistentEventCount() < 1)
-            {
-                _events.Remove(name);
-            }
+            // grab the handler and subscribe
+            var handler = (EventHandler<T>)_genericHandlers[typeof(T)];
+            handler.Subscribe(key, func);
         }
-
-        public static bool Invoke(string name)
+    
+        public static void Unsubscribe<T>(string key, Action<T> func )
         {
-            // return false if no exist
-            if (!_events.ContainsKey(name)) return false;
-            
-            // invoke event and return true
-            _events[name].Invoke();
-            return true;
+            // do nothing if no handler
+            if (!_genericHandlers.ContainsKey(typeof(T))) return;
+        
+            // grab the handler and unsubscribe
+            var handler = (EventHandler<T>)_genericHandlers[typeof(T)];
+            handler.Unsubscribe(key, func);
+        }
+    
+        public static void Invoke<T>(string key, T args)
+        {
+            // do nothing if no handler
+            if (!_genericHandlers.ContainsKey(typeof(T))) return;
+        
+            // grab the handler and do thing
+            var handler = (EventHandler<T>)_genericHandlers[typeof(T)];
+            handler.Invoke(key, args);        
         }
     }
-    
 }
