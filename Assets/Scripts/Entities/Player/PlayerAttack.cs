e0a31aa74486c7c2ec16213393a90f6a7077b700
Alex Wai Yiu Wong <aw987123@gmail.com>
2021-12-07 12:36:50 +0000

    Modified Attack Ability to damage entities, no coroutines yet

--- Assets/Scripts/Entities/Player/PlayerAttack.cs
+++ Assets/Scripts/Entities/Player/PlayerAttack.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    
+    public bool IsUsingSkill;
+
+    private Coroutine _skillCoroutine = null;
+
+    public void AttackEnemy(Player player, GameObject tileObject)
+    {
+        
+    }
 }
